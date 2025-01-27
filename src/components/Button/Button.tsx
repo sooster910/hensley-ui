f5beac2e784c2c440401722a4fdd7ea403089eba
Hyunsu <hs9880@gmail.com>
2025-01-27 11:11:45 +0900

    feat: Configure project for library distribution
    - Update Vite configuration for library build
    - Modify package.json for module and type exports
    - Remove default Vite React template files
    - Add .history to .gitignore
    - Prepare project for component library distribution

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,6 @@
+import { PropsWithChildren } from 'react'
+
+
+export const Button= ({children}:PropsWithChildren)=>{
+    return <button>{children}</button>
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import dts from "vite-plugin-dts"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),// React JSX를 JavaScript로 변환
+    dts({ insertTypesEntry: true }) // TypeScript 타입 정의 파일(.d.ts) 생성
+  ],
+  build:{
+    lib:{
+      entry: resolve(__dirname, 'src/index.ts'),
+      name:"hensley-ui",
+      formats:['es','cjs','umd'],
+      fileName:(format) => `hensley-ui.${format}.js`,
+    },
+    rollupOptions:{
+      external:["react","react-dom"],
+      output:{
+        globals:{ 
+          react:"React",
+          "react-dom":"ReactDOM" 
+        }
+      }
+    }
+  }
 })
