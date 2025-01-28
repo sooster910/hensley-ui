b156bcc0c3fa0404ffdd33fe8abe85ccf722323b
Hyunsu <hs9880@gmail.com>
2025-01-29 01:51:19 +0900

    chore: Complete setup for library build with symlink testing

---
+++ hensley-ui/src/index.ts
@@ -0,0 +1,2 @@
+export type { ButtonProps } from './components/Button/Button';
+export { Button } from './components/Button/Button';
--- src/components/Button/Button.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from 'react'
-
-
-export const Button= ({children}:PropsWithChildren)=>{
-    return <button>{children}</button>
-}
