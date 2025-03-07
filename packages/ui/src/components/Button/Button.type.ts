6cff58d4e3ee9fed46d4adbc840201c225e47565
Hyunsu <hs9880@gmail.com>
2025-03-07 13:23:29 +0900

    fix(UI): 버튼 경로 재설정

--- packages/ui/src/components/Button/Button.type.ts
+++ packages/ui/src/components/Button/Button.type.ts
@@ -1,5 +1,5 @@
 import { VariantProps } from 'class-variance-authority'
-import { buttonVariants } from './Button'
+import { buttonVariants } from '@/components/button/Button'
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
