4dd77f8e73cc08f7ea6895e89aafae04a10a1ed4
Hyunsu <hs9880@gmail.com>
2025-04-15 19:57:01 +0900

    chore: vitest alias 읽기 설정 및  테스트 확인

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vitest/config'
+import path from 'node:path'
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
